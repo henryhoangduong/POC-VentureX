52099c1ef718a064c5c48dcaf3dbaa2becf19158
henryhoangduong <henryhoangduong@gmail.com>
2024-06-25 13:25:45 +0700

    comment eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+// module.exports = {
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     ecmaVersion: 2021,
+//     sourceType: 'module',
+//   },
+//   settings: {
+//     react: {
+//       version: 'detect',
+//     },
+//   },
+//   extends: [
+//     'plugin:react/recommended',
+//     'plugin:react-hooks/recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:import/errors',
+//     'plugin:import/warnings',
+//     'plugin:import/typescript',
+//     'plugin:jsx-a11y/recommended',
+//     'plugin:eslint-comments/recommended',
+//     'plugin:react/jsx-runtime',
+//     'plugin:prettier/recommended',
+//   ],
+//   rules: {
+//     'no-unused-vars': 'off',
+//     '@typescript-eslint/no-unused-vars': ['error'],
+//     '@typescript-eslint/no-var-requires': 'off',
+//     'react/prop-types': 'off',
+//     '@typescript-eslint/explicit-module-boundary-types': 'off',
+//     'react-hooks/rules-of-hooks': 'error',
+//     'react-hooks/exhaustive-deps': 'warn',
+//     'import/no-named-as-default': 0,
+//   },
+//   ignorePatterns: ['webpack/*', 'eslintre.js'],
+// }
